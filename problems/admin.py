f0f732fe22ec9e43de3ddf16b5bd4830a5f7640c
Anton <antonderegt@pm.me>
2020-05-03 14:22:16 +0200

    Update admin page

--- problems/admin.py
+++ problems/admin.py
@@ -1,24 +1,7 @@
 from django.contrib import admin
-
-# Register your models here.
 from problems.models import Problem
 from problems.models import Category
 from problems.models import Item
-
-# class ChoiceInline(admin.TabularInline):
-#     model = Choice
-#     extra = 3
-
-# class QuestionAdmin(admin.ModelAdmin):
-#     fieldsets = [
-#         (None,               {'fields': ['question_text']}),
-#         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-#     ]
-#     inlines = [ChoiceInline]
-#     list_display = ('question_text', 'pub_date', 'was_published_recently')
-#     list_filter = ['pub_date']
-#     search_fields = ['question_text']
-
 
 class ItemInline(admin.TabularInline):
     model = Item
@@ -27,16 +10,22 @@ class ItemInline(admin.TabularInline):
 class CategoryInline(admin.TabularInline):
     model = Category
     extra = 0
-    inlines = [ItemInline]
 
 class ProblemAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,        {'fields': ['problem']}),
-        # ('Date info', {'fields': ['createdAt']}),
+        (None,          {'fields': ['problem']}),
+        (None,          {'fields': ['description']}),
+        # ('Date info',   {'fields': ['createdAt']}),
     ]
     inlines = [CategoryInline]
     list_filter = ['createdAt']
     search_fields = ['problem']
 
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,          {'fields': ['category']}),
+    ]
+    inlines = [ItemInline]
+
 admin.site.register(Problem, ProblemAdmin)
-admin.site.register(Item)
+admin.site.register(Category, CategoryAdmin)
