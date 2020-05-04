8229a26b51a54a3101e6b692b32b51825c25c247
Anton <antonderegt@pm.me>
2020-05-04 16:34:06 +0200

    Implement problem detail api view

--- problems/views.py
+++ problems/views.py
@@ -59,7 +59,7 @@ def problem_detail(request, pk):
         serializer = ProblemSerializer(problem, data=request.data,context={'request': request})
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
